87d0cdf69761e1801cba497154fd38c04a70e0e3
Remy <remy93130@gmail.com>
2019-01-16 23:50:37 +0100

    fix manyTomany bug on form

--- src/Form/PersonnalType.php
+++ src/Form/PersonnalType.php
@@ -10,6 +10,9 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use App\Entity\Role;
 use App\Entity\Specialization;
 use App\Entity\HospitalNode;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class PersonnalType extends AbstractType
 {
@@ -18,20 +21,21 @@ class PersonnalType extends AbstractType
         $builder
             ->add('surname')
             ->add('lastname')
-            ->add('email')
-            ->add('password')
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
             ->add('role', EntityType::class, [
                 'class' => Role::class,
                 'choice_label' => 'name'
             ])
-//             ->add('specialization', EntityType::class, [
-//                 'class' => Specialization::class,
-//                 'choice_label' => 'name'
-//             ])
+             ->add('specialization', EntityType::class, [
+                 'class' => Specialization::class,
+                 'choice_label' => 'name',
+                 'multiple' => true,
+             ])
             // ->add('hospitalNode')
             ->add('assignment', EntityType::class, [
                 'class' => HospitalNode::class,
-                'choice_label' => 'name'
+                'choice_label' => 'name',
             ])
         ;
     }
@@ -40,6 +44,8 @@ class PersonnalType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => Personnal::class,
+            'translation_domain' => 'form_personnal',
+            'allow_delete' => true,
         ]);
     }
 }
