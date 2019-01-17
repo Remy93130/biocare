815fb68df2e86d414b44f4f90f29f8a67bf83e06
Remy93130 <remy93130@gmail.com>
2019-01-17 23:54:13 +0100

    début recherche DMP

--- src/Controller/DoctorController.php
+++ src/Controller/DoctorController.php
@@ -7,19 +7,33 @@ use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Acts;
 use Symfony\Component\HttpFoundation\Request;
+use App\Entity\DMP;
 
 class DoctorController extends AbstractController
 {
     /**
      * @Route("/home/acts", name="doctor_acts")
      */
-    public function getActs(Request $request, EntityManagerInterface $em): Response {
+    public function getActs(EntityManagerInterface $em): Response
+    {
         $db = $em->getRepository(Acts::class);
         $acts = $db->getDoctorUnifinishedActs($this->getUser()->getId());
         // echo $request->headers->get("referer");
-        return $this->render("main/doctor/search.html.twig", [
+        return $this->render("main/doctor/acts.html.twig", [
             'acts' => $acts,
         ]);
+    }
+    
+    /**
+     * @Route("home/search", name="doctor_search")
+     */
+     public function searchDMP(EntityManagerInterface $em): Response  
+    {
+         $db = $em->getRepository(DMP::class);
+         $dmps = $db->findAll();
+         return $this->render("main/doctor/search.html.twig", [
+             "dmps" => $dmps,
+         ]);
     }
 }
 
